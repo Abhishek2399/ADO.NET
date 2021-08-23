0dab7107c15ade9cd53b53900cc2a6fc1af0e53a
Abhishek <abhibhovar@gmail.com>
2021-08-23 16:15:17 +0530

    ADO.NET Modified

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -12,9 +12,13 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            // As our provider is SQL Server we need SQL connection 
+            // As our provider is SQL Server we use SqlConnection class
             SqlConnection con = new SqlConnection();
-            
+            con.ConnectionString = "server=NOOB;database=DemoProj;trusted_connection=true"; // SQL server information we want to connect 
+            // 'server=' name of the server
+            // 'database=' name of the database we want to use
+            // 'trusted_connection =  true' for windows authentication 
+
         }
     }
 }
