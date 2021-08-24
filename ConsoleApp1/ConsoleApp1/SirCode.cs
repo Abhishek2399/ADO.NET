3dcf83aec22b071f740fc97328474cf536dd6603
Abhishek <abhibhovar@gmail.com>
2021-08-24 08:39:44 +0530

    Modified

--- ConsoleApp1/ConsoleApp1/SirCode.cs
+++ ConsoleApp1/ConsoleApp1/SirCode.cs
@@ -15,11 +15,7 @@ namespace ado1
             ////cmd.CommandText = "select * from emps";
             ////cmd.Connection = condb;
 
-
-
             //SqlDataReader red=cmd.ExecuteReader();
-
-
 
             //while(red.Read()==true)
             //{
@@ -28,71 +24,38 @@ namespace ado1
             //    Console.WriteLine("------------------------");
             //}
 
-
-
-
             // to insert record
-
-
 
             //SqlCommand insertcmd = new SqlCommand("insert into simp values(1,'ravi')");
             //insertcmd.Connection = condb;
 
 
-
             //insertcmd.ExecuteNonQuery();
             //Console.WriteLine("record inserted...");
-
-
-
             // to delete record
-
-
-
             //SqlCommand delcmd = new SqlCommand("delete from simp where rno =1");
             //delcmd.Connection = condb;
 
-
-
             //int r = delcmd.ExecuteNonQuery();
-
-
 
             //if (r == 0)
             //    Console.WriteLine("record not found");
             //else
             //    Console.WriteLine("record deleted...");
 
-
-
             // to update record
-
-
 
             SqlCommand upcmd = new SqlCommand("update simp set sname='newname' where rno =1");
             upcmd.Connection = condb;
 
-
-
             int r = upcmd.ExecuteNonQuery();
-
-
 
             if (r == 0)
                 Console.WriteLine("record not found");
             else
                 Console.WriteLine("record updated...");
 
-
-
-
-
-
             condb.Close();
-
-
-
-
 
         }
     }
