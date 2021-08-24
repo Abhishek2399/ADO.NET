56c6f6e9e9543350f0ed55df291eb0a22d5a7767
Abhishek <abhibhovar@gmail.com>
2021-08-24 09:42:57 +0530

    Modified

---
+++ UserDrivenQueries/UserDrivenQueries/UserDriven.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+namespace UserDrivenQueries
+{
+    class UserDriven
+    {
+        static void Main(string[] args)
+        {
+            //------------- <SqlConnection Establishment> ------------- 
+            SqlConnection con = new SqlConnection("Data Source = NOOB; Initial Catalog = ADODay2; Integrated Security = true");
+            // "Data Source" : Server where our db is present 
+            // "Initial Catalog" : Name of the Db
+            // "Integrated Security" : Windows Authentication / manual user name / password 
+
+            con.Open();
+            Console.WriteLine("Connection Successful");
+            //---------------------<>----------------------------------
+
+
+        }
+    }
+}
