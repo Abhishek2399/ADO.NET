ae068f8b85af359ee2b4bf331dd823e0b781d38d
Abhishek <abhibhovar@gmail.com>
2021-08-24 18:15:40 +0530

    Modified

--- ConnectionLess/ConnectionLess/Program.cs
+++ ConnectionLess/ConnectionLess/Program.cs
@@ -9,8 +9,28 @@ namespace ConnectionLess
     {
         static SqlConnection con = new SqlConnection();
         static SqlCommand cmd = new SqlCommand();
-        static SqlDataAdapter adapt = new SqlDataAdapter(); // to fill data in the data set
+        static SqlDataAdapter da = new SqlDataAdapter(); // to fill data in the data set
         public static DataSet ds = new DataSet(); // for locally storing data 
+
+
+        public static void DisplaySpecificData()
+        {
+            cmd.Connection = con;
+            cmd.CommandText = "select * from Users where uid = @uname";
+            cmd.Parameters.AddWithValue("@uname", "Admin");
+
+            da.SelectCommand = cmd;
+            da.Fill(ds, "LocalUser");
+
+            for (int i = 0; i < ds.Tables["LocalUser"].Rows.Count; i++)
+            {
+                Console.WriteLine(ds.Tables["LocatUser"].Rows[i][0]); // first row first col
+                Console.WriteLine(ds.Tables["LocalUser"].Rows[i][1]); // first row second col
+                Console.WriteLine("--------------------------");
+            }
+
+
+        }
 
         static void Main(string[] args)
         {
@@ -19,9 +39,11 @@ namespace ConnectionLess
             
             cmd.Connection = con;
             cmd.CommandText = "select * from Users";
+            
+            SqlCommandBuilder cmdbuilder = new SqlCommandBuilder(da);// will generate the Queries for the adapter 
 
-            adapt.SelectCommand = cmd; // get the data from the select command 
-            adapt.Fill(ds); // fill the data set
+            da.SelectCommand = cmd; // get the data from the select command 
+            da.Fill(ds); // fill the data set
 
             con.Close(); // closing the connection of the data base
             // even tho we can see the output which is because of the data set
@@ -33,9 +55,22 @@ namespace ConnectionLess
             Console.WriteLine(ds.Tables[0].Rows[0][0]); // first row first col
             Console.WriteLine(ds.Tables[0].Rows[0][1]); // first row second col
 
+            for(int i = 0; i< ds.Tables[0].Rows.Count; i++)
+            {
+                Console.WriteLine(ds.Tables[0].Rows[i][0]); // first row first col
+                Console.WriteLine(ds.Tables[0].Rows[i][1]); // first row second col
+                Console.WriteLine("--------------------------");
+            }
+
+            DataRow newr = ds.Tables[0].NewRow();
+
+            newr[0] = "someone";
+            newr[1] = "Someon123";
+
+            ds.Tables[0].Rows.Add(newr); 
 
 
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
