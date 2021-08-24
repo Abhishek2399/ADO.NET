d1ac00b3bc6afce96e900aa15b1d9a03409b2fb5
Abhishek <abhibhovar@gmail.com>
2021-08-24 09:45:50 +0530

    Modified

--- UserDrivenQueries/UserDrivenQueries/UserDriven.cs
+++ UserDrivenQueries/UserDrivenQueries/UserDriven.cs
@@ -8,16 +8,22 @@ namespace UserDrivenQueries
     {
         static void Main(string[] args)
         {
-            //------------- <SqlConnection Establishment> ------------- 
-            SqlConnection con = new SqlConnection("Data Source = NOOB; Initial Catalog = ADODay2; Integrated Security = true");
-            // "Data Source" : Server where our db is present 
-            // "Initial Catalog" : Name of the Db
-            // "Integrated Security" : Windows Authentication / manual user name / password 
+            try
+            {
+                //------------- <SqlConnection Establishment> ------------- 
+                SqlConnection con = new SqlConnection("Data Source = NOOB; Initial Catalog = ADODay2; Integrated Security = true");
+                // "Data Source" : Server where our db is present 
+                // "Initial Catalog" : Name of the Db
+                // "Integrated Security" : Windows Authentication / manual user name / password 
 
-            con.Open();
-            Console.WriteLine("Connection Successful");
-            //---------------------<>----------------------------------
-
+                con.Open();
+                Console.WriteLine("Connection Successful");
+                //---------------------<>----------------------------------
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
         }
     }
